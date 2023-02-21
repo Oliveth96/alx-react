980b6341c0cb2a8ae852fbbc7de2561a520be990
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2023-02-21 17:28:48 +0100

    Modified the App

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,32 @@
+/* eslint-disable jsx-a11y/alt-text */
+import logo from "./holbertonLogo.jpg";
+import { getFooterCopy } from "./utils";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App-header">
+        <img src={logo} />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email"> Email
+            <Input type="email" id="email" />
+        </label>
+
+        <label htmlFor="password"> Password
+            <Input type="text" id="password" />
+        </label>
+
+        <button>OK</button>
+      </div>
+      <div className="App-footer">
+        <p>{getFooterCopy()}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;

a70e402f5c0b3014e723b418209e8a211a4c0f70
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2023-02-21 17:11:50 +0100

    Embedding expressions and functions

---
+++ 0x01-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,7 @@
+export const getFullYear = () => {
+    return new Date().getFullYear();
+  };
+  export const getFooterCopy = () => {
+    return `© ${getFullYear()} Holberton School main dashboard`;
+  };
+  

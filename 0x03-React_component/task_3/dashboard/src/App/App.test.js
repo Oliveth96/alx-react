cdc4b3345f6ef949a0a930a2e99d7c1920a7f955
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2023-03-02 13:00:10 +0100

    Reusable comments & specialization

--- 0x03-React_component/task_3/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_3/dashboard/src/App/App.test.js
@@ -1,87 +1,61 @@
-import React, { Component } from "react";
-import Notifications from "../Notifications/Notifications";
-import Header from "../Header/Header";
-import BodySection from "../BodySection/BodySection";
-import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
-import Login from "../Login/Login";
-import CourseList from "../CourseList/CourseList";
-import Footer from "../Footer/Footer";
-import PropTypes from "prop-types";
-import { getLatestNotification } from "../utils/utils";
-import "./App.css";
+import { shallow, mount } from "enzyme";
+import React from "react";
+import App from "./App";
 
-const listCourses = [
-  { id: 1, name: "ES6", credit: 60 },
-  { id: 2, name: "Webpack", credit: 20 },
-  { id: 3, name: "React", credit: 40 },
-];
+describe("<App />", () => {
+  it("App renders without crashing", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists()).toEqual(true);
+  });
+  it("should contain the Notifications component", () => {
+    const wrapper = shallow(<App />);
+    wrapper.update();
+    expect(wrapper.find("Notifications")).toHaveLength(1);
+  });
+  it("should contain the Header component", () => {
+    const wrapper = shallow(<App />);
+    wrapper.update();
+    expect(wrapper.find("Header")).toHaveLength(1);
+  });
+  it("should contain the Login component", () => {
+    const wrapper = shallow(<App />);
+    wrapper.update();
+    expect(wrapper.find("Login")).toHaveLength(1);
+  });
+  it("should contain the Footer component", () => {
+    const wrapper = shallow(<App />);
+    wrapper.update();
+    expect(wrapper.find("Footer")).toHaveLength(1);
+  });
+  it("CourseList is not displayed with isLoggedIn false by default", () => {
+    const wrapper = shallow(<App />);
+    wrapper.update();
+    expect(wrapper.find("CourseList")).toHaveLength(0);
+  });
+  it("isLoggedIn is true", () => {
+    const wrapper = shallow(<App isLoggedIn />);
+    wrapper.update();
+    expect(wrapper.find("Login")).toHaveLength(0);
+    expect(wrapper.find("CourseList")).toHaveLength(1);
+  });
 
-const listNotifications = [
-  { id: 1, type: "default", value: "New course available" },
-  { id: 2, type: "urgent", value: "New resume available" },
-  { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
-];
+  it("when the keys control and h are pressed the logOut function, passed as a prop, is called and the alert function is called with the string Logging you out", () => {
+    const events = {};
+    const logout = jest.fn();
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.handleKeyCombination = this.handleKeyCombination.bind(this);
-  }
+    document.addEventListener = jest.fn((event, cb) => {
+      events[event] = cb;
+    });
 
-  handleKeyCombination(e) {
-    if (e.key === "h" && e.ctrlKey) {
-      alert("Logging you out");
-      this.props.logOut();
-    }
-  }
+    window.alert = jest.fn();
 
-  componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyCombination);
-  }
+    shallow(<App logOut={logout} />);
 
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleKeyCombination);
-  }
+    events.keydown({ key: "h", ctrlKey: true });
 
-  render() {
-    const { isLoggedIn, logOut } = this.props;
-    return (
-      <>
-        <Notifications listNotifications={listNotifications} />
-        <div className="App">
-          <Header />
-        </div>
-        <div className="App-body">
-          {!isLoggedIn ? (
-            <BodySectionWithMarginBottom title="Log in to continue">
-              <Login />
-            </BodySectionWithMarginBottom>
-          ) : (
-            <BodySectionWithMarginBottom title="Course list">
-              <CourseList listCourses={listCourses} />
-            </BodySectionWithMarginBottom>
-          )}
-        </div>
-        <BodySection title="News from the School">
-          <p>Text</p>
-        </BodySection>
+    expect(window.alert).toHaveBeenCalledWith("Logging you out");
+    expect(logout).toHaveBeenCalled();
 
-        <div className="App-footer">
-          <Footer />
-        </div>
-      </>
-    );
-  }
-}
-
-App.defaultProps = {
-  isLoggedIn: false,
-  logOut: () => {},
-};
-
-App.propTypes = {
-  isLoggedIn: PropTypes.bool,
-  logOut: PropTypes.func,
-};
-
-export default App;
+    jest.restoreAllMocks();
+  });
+});
